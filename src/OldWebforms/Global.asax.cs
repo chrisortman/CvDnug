000788bb20debd4de19e1d641841e3dd612e705a
Chris Ortman <chriso@innovsys.com>
2011-09-07 16:53:29 -0500

    Merged mvc into webforms project

--- src/OldWebforms/Global.asax.cs
+++ src/OldWebforms/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -13,7 +15,10 @@ namespace OldWebforms
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
 
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
         }
 
         void Application_End(object sender, EventArgs e)
@@ -42,6 +47,21 @@ namespace OldWebforms
             // or SQLServer, the event is not raised.
 
         }
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
 
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
     }
 }
