a91c20685cd684313cf6de4bf43a520ad7803e13
Chris Ortman <chrisortman@gmail.com>
2011-09-07 22:23:08 -0500

    Renaming for to better present with

---
+++ src/OldWebforms/Lib/01_JavascriptIncludes.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace OldWebforms.Helpers
+{
+    public static class ContentHelper
+    {
+         public static string JavascriptIncludes()
+         {
+             var scriptFiles = new[]
+             {
+                 "jquery-1.6.2.min.js",
+                 "jquery-1.8.16.min.js",
+                 "jquery-unobtrusive-ajax.min.js",
+                 "jquery.validate.min.js",
+                 "jquery.validate.unobtrusive.js",
+                 "modernizr-2.0.6-development-only.js"
+             };
+
+             var sb = new StringBuilder();
+             foreach(var script in scriptFiles)
+             {
+                 sb.AppendFormat("<script src=\"/scripts/{0}\" type=\"text/javascript\"></script>\n",script);
+             }
+
+             return sb.ToString();
+         }
+
+         public static IHtmlString JavascriptIncludes(this HtmlHelper html)
+         {
+             return new MvcHtmlString(JavascriptIncludes());
+         }
+    }
+
+    public class JavascriptIncludesControl : WebControl
+    {
+        protected override void Render(System.Web.UI.HtmlTextWriter writer)
+        {
+            writer.Write(ContentHelper.JavascriptIncludes());
+        }
+    }
+}
