dc2f6bd239e78ff58d2828577becf1e076e31da5
Chris Ortman <chrisortman@gmail.com>
2011-09-08 00:25:15 -0500

    Add test for about route

---
+++ src/Tests/RouteTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Ketchup.Mvc.Testing;
+using Ketchup.Web.Routing;
+using NUnit.Framework;
+using OldWebforms;
+using Shouldly;
+
+namespace Tests
+{
+    public class RouteTests : RouteTestBase
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            RouteTable.Routes.Clear();
+            Global.RegisterRoutes(RouteTable.Routes);
+
+        }
+
+
+        [Test]
+        public void AboutRoute()
+        {
+            Url.RouteUrl("AboutUrl").ShouldBe("/About.aspx");
+            "~/About.aspx".ShouldMapToWebFormsPage();
+        }
+    
+    }
+}
